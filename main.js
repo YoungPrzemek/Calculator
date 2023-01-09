2c71fc162bfc232649d23c84b77b4eca4e8883c6
YoungPrzemek <przemek1087b@gmail.com>
2023-01-09 17:52:06 +0100

    Change class

--- main.js
+++ main.js
@@ -1,76 +1,17 @@
 //Pobieramy elementy
-const currentNumber = document.querySelector('.currentNumber');
-
-const previousNumber = document.querySelector('.previousNumber p');
-
-const mathSign = document.querySelector('.mathSign');
-
-const numbersButtons = document.querySelectorAll('.number');
-
-const operatorsButtons = document.querySelectorAll('.operator');
-
-const equalsButton = document.querySelector('.equals');
-const clearAll = document.querySelector('.clearAll');
-const clearOne = document.querySelector('.clearOne');
+const number = document.querySelectorAll('.number')
+const operate = document.querySelectorAll('.operate')
+const clearAll= document.querySelector('.clear-all')
+const clearOne = document.querySelector('.clear-one')
+const equales = document.querySelector('.equales')
+const previousNumber = document.querySelector('.previous-number')
+const currentScore = document.querySelector('.current-score')
 
 
- let result = '';
+
 //Funckcje 
-
-function dispalyNumbers(){
-    if(this.textContent === '.' && currentNumber.innerHTML.includes('.')) return;
-    if(this.textContent === '.' && currentNumber.innerHTML === '') return currentNumber.innerHTML = '.0'
-
-    currentNumber.innerHTML += this.textContent;
-}
-
-function operate(){
-     if(currentNumber.innerHTML === '' && this.textContent === '-'){
-     currentNumber.innerHTML = '-';
-     return;
-     }
-     
-     else if (currentNumber.innerHTML === '') {
-         return;
-     }
-
-     if(mathSing.innerHTML!== ''){
-         showResult();
-     }
-     previousNumber.innerHTML += currentNumber.innerHTML;
-     mathSing.innerHTML = this.textContent;
-     currentNumber.innerHTML = '';
-}
-  
-
-
-function showResult () {
-    
-}
-
-function clearScreen () {
-
-}
-
-function clearNumber () {
-
-}
-
-
-
-
-
-
 
 
 
 
 //Nasłuchiwanie na przyciski
-operatorsButtons.forEach((button) => button.addEventListener('click', operate))
-
- equalsButton.addEventListener('click', showResult);
-
-
- numbersButtons.forEach((button) => {
-     button.addEventListener('click', dispalyNumbers)
- })
